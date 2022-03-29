2494997bb04be6f9197403fefa10a7f656997955
DarkedgeTH <darkedgeth@gmail.com>
2022-03-29 16:08:29 -0400

    set up AddFriend

--- src/components/AddFriend.js
+++ src/components/AddFriend.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React from 'react';
 
 const AddFriend = () => {
-    return(<h2>AddFriend</h2>)
+    return(<div>
+        <h2>Add Friend</h2>
+        <form>
+            <div>
+                <label htmlFor='username'>Username:</label>
+                <input id='username'/>
+            </div>
+            <div>
+                <label htmlFor='age'>Age:</label>
+                <input id='age'/>
+            </div>
+            <div>
+                <label htmlFor='email'>Email:</label>
+                <input id='email'/>
+            </div>
+            <button>Submit</button>
+        </form>
+    </div>)
   }
 
 export default AddFriend
--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react';
 
 const FriendsList = () => {
     return(<div>
-        <h2>FriendsList</h2>
+        <h2>Friends List</h2>
         <ul>
             <li>Name - age - email</li>
             <li>Name - age - email</li>
