6dd1ca77b3484378db6fc802de204d86f49139a1
DarkedgeTH <darkedgeth@gmail.com>
2022-03-29 16:39:15 -0400

    added and initiated onChange and onSubmit for Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const Login = () => {
+    const [cred, setCred] = useState({
+        username: '',
+        password: ''
+    })
+
+    const handleChange = (evt) => {
+        setCred({
+            ...cred,
+            [evt.target.name]: evt.target.value
+        })
+    }
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault()
+        axios.post('http://localhost:9000/api/login', cred)
+            .then(res => {
+                localStorage.setItem('token', res.payload)
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    }
+
     return(<div>
       <h1>Login</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor='username'>Username:</label>
-          <input id='username' />
+          <input onChange={handleChange} name='username' id='username' />
         </div>
         <div>
           <label htmlFor='password'>Password:</label>
-          <input type='password' id='password' />
+          <input onChange={handleChange} name='password' type='password' id='password' />
         </div>
         <button>Submit</button>
       </form>
